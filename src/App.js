a665f17c7eb1e9938bc4d860109fd96df4496e28
luciano conti <conti98.luciano@gmail.com>
2024-03-18 04:12:07 -0300

    Boton agregado y obtiene datos de la API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Weather from './components/weather';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -3,23 +3,26 @@ import React, { useState } from 'react';
 
 const Weather = () => {
 
-  const [city, setCity] = useState("Delhi")
-
+  const [city, setCity] = useState("Rosario");
+  const [country, setCountry] = useState("AR");
   const getWeatherData = async(city,country) =>{
     await axios({
       method: 'GET',
-      url:'https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}'
+      url:`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=85d978259d59a3e72dcf14251e348607`
     }).then((res) =>{
       console.log(res.data);
     }).catch((err) =>{
-      console.log(err)
+      console.log(err);
     })
   }
   return (
-    <div>
-      <h1>hola</h1>
-      <h1>{city}</h1>
+    <div  className="container">
+    <h1>Aplicacion del Clima</h1>
+      <input type="text" value={city} onChange={(e) => setCity(e.target.value)}  placeholder='Ciudad'/>
+      <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder='Pais'  />
+      <button onClick={() => getWeatherData(city,country)} className='btn btn-primary'>Obtener Clima</button>
     </div>
+
   )
 }
 
